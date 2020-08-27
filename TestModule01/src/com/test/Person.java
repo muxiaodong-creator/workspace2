6125e9354342198ca8ccfbfbfe1b37ba6086f0a1
muxd <muxd@yonyou.com>
2020-08-27 19:29:33 +0800

    person增加name age 相关属性

---
+++ TestModule01/src/com/test/Person.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * program: workspace3
+ * description：com.test
+ * author: muxd
+ * create: 2020-08-27 19:28
+ **/
+public class Person {
+    /** 年龄 */
+    private String age;
+}
